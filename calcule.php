624e83d0f43b1c9189bad478f9dee4c9b0b2e736
Yasserkdr1 <kdryassir@gmail.com>
2025-03-28 15:44:26 +0000

    first commit

---
+++ calcule.php
@@ -0,0 +1,15 @@
+<?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $n = $_POST['number'];
+
+        echo "<h2>Résultat :</h2>";
+
+        for ($i = 1; $i <= $n; $i++) {
+            if ($i % 2 == 0) {
+                echo "$i est pair<br>";
+            } else {
+                echo "$i est impair<br>";
+            }
+        }
+    }
+    ?>
